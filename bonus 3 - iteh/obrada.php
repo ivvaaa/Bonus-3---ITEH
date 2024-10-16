9b63bc131ca71fcd49eafd586ec3aaf6d39f6c72
ivvaaa <98695607+ivvaaa@users.noreply.github.com>
2024-10-16 13:12:45 +0200

    Add files via upload

---
+++ bonus 3 - iteh/obrada.php
@@ -0,0 +1,13 @@
+<?php
+    require "dbBroker.php";
+    require "model/prijava.php";
+    require "handler/delete.php";
+    require "handler/add.php";
+    require "handler/update.php";
+
+
+    // echo "obrada..";
+
+    header("Location: home.php");
+
+?>
